53dbc19a61f9b53ad4144c62f537af69606085d1
=Alisha Soedamah <=Alisha.Soedamah@student.rocva.nl>
2020-10-05 20:48:01 +0200

    console ans

---
+++ 05 - Javascript/taak02 - Op welke type let jij/script.js
@@ -0,0 +1,13 @@
+
+let mijnVoornaam = "Alisha"
+let mijnAchternaam = 'Soedamah'
+let leeftijd = 16;
+let ikBenStemGerechtig = false;
+let ikMagBrommerRijden = true;
+let SoftwareOntwikkeling = true;
+console.log(mijnVoornaam);
+console.log(mijnAchternaam);
+console.log(leeftijd);
+console.log(ikBenStemGerechtig);
+console.log(ikMagBrommerRijden);
+console.log(SoftwareOntwikkeling);
