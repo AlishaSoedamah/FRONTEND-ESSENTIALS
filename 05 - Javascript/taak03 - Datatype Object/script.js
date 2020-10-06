ce1f2f18ef123ecf7feada127d73d680dc48e8a0
=Alisha Soedamah <=Alisha.Soedamah@student.rocva.nl>
2020-10-06 10:43:27 +0200

    console.log(student);

---
+++ 05 - Javascript/taak03 - Datatype Object/script.js
@@ -0,0 +1,11 @@
+let student = {
+    mijnVoornaam: "Alisha",
+    mijnAchternaam:"Soedamah" ,
+    leeftijd: 16 ,
+    ikBenStemGerechtig: false ,
+    ikMagBrommerRijden: true ,
+    SoftwareOntwikkeling: true
+}
+console.log(student);
+
+
