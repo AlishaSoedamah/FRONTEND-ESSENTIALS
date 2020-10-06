e768f8a215ee395876a36325ed697db818d5c5ac
=Alisha Soedamah <=Alisha.Soedamah@student.rocva.nl>
2020-10-06 12:20:40 +0200

    js

--- 05 - Javascript/taak05 - As if/script.js
+++ 05 - Javascript/taak05 - As if/script.js
@@ -2,6 +2,8 @@ let mijnLeeftijd = 16;
 let stemGerechtigeleeftijd = 18;
 if(mijnLeeftijd == stemGerechtigeleeftijd){
     console.log("Ja");
+   alert("Jij mag stemmen met de verkiezingen")
+   alert("Ja")
 } else {
    console.log("Nee");
    alert("Jij mag stemmen met de verkiezingen")
---
+++ 05 - Javascript/taak06 - If  something else/script.js
@@ -0,0 +1,13 @@
+let stopLichtKleur = "rood";
+
+if (stopLichtKleur==rood){
+    console.log(true);
+    
+    alert("Jij moet stoppen voor het stoplicht")
+} 
+else{
+    console.log(false);
+    alert("Je mag doorrijden.")
+}
+
+
