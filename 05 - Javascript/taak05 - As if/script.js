41462d6632308b96f0239e284b76378b427fcdbe
=Alisha Soedamah <=Alisha.Soedamah@student.rocva.nl>
2020-10-06 11:56:11 +0200

    As if

---
+++ 05 - Javascript/taak05 - As if/script.js
@@ -0,0 +1,10 @@
+let mijnLeeftijd = 16;
+let stemGerechtigeleeftijd = 18;
+if(mijnLeeftijd == stemGerechtigeleeftijd){
+    console.log("Ja");
+} else {
+   console.log("Nee");
+   alert("Jij mag stemmen met de verkiezingen")
+   alert("Nee")
+}
+
