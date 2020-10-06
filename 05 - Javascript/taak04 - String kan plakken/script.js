efbcd1ad46f39b4d516f0ffbc58c31ab5c3a2589
=Alisha Soedamah <=Alisha.Soedamah@student.rocva.nl>
2020-10-06 11:12:17 +0200

    console

---
+++ 05 - Javascript/taak04 - String kan plakken/script.js
@@ -0,0 +1,10 @@
+
+let mijnVoornaam = "Alisha";
+let mijnAchternaam ='Soedamah';
+let mijnLeeftijd = 16;
+
+let volledigeZin = "Mijn naam is"+ mijnVoornaam + " " + mijnAchternaam + "en mijn leeftijd is" + mijnLeeftijd + "jaar";
+
+console.log = (volledigeZin);
+
+
