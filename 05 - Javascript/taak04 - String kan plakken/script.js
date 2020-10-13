fe34925dea55d1213695f71141477039f42fb311
=Alisha Soedamah <=Alisha.Soedamah@student.rocva.nl>
2020-10-13 21:50:25 +0200

    KLAAR

--- 05 - Javascript/taak04 - String kan plakken/script.js
+++ 05 - Javascript/taak04 - String kan plakken/script.js
@@ -2,6 +2,6 @@ let mijnVoornaam = "Alisha";
 let mijnAchternaam = 'Soedamah';
 let leeftijd = 16;
 
-let volledigeZin = "Mijn naam is " + mijnVoornaam + " " +  mijnAchternaam + "en mijn leeftijd is " + leeftijd + " jaar";
+let volledigeZin = "Mijn naam is " + mijnVoornaam + " " +  mijnAchternaam + " " + "en mijn leeftijd is " + leeftijd + " jaar";
 console.log(volledigeZin);
 
--- 05 - Javascript/taak06 - If  something else/script.js
+++ 05 - Javascript/taak06 - If  something else/script.js
@@ -1,12 +1,10 @@
 let stopLichtKleur = "rood";
 
 if (stopLichtKleur==rood){
-    console.log(true);
-    
+
     alert("Jij moet stoppen voor het stoplicht")
 } 
 else{
-    console.log(false);
     alert("Je mag doorrijden.")
 }
 
