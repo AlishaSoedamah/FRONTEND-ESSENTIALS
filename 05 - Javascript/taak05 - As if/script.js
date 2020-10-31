1fde73cb6499cbce0d2d4e6eb8ae2959d32eb310
=Alisha Soedamah <=Alisha.Soedamah@student.rocva.nl>
2020-10-31 17:04:57 +0100

    Mondriaan AF

--- 05 - Javascript/taak05 - As if/script.js
+++ 05 - Javascript/taak05 - As if/script.js
@@ -1,11 +1,13 @@
 let mijnLeeftijd = 16;
 let stemGerechtigeleeftijd = 18;
+
 if(mijnLeeftijd == stemGerechtigeleeftijd){
-    console.log("Ja");
+    console.log(true);
    alert("Jij mag stemmen met de verkiezingen")
    alert("Ja")
-} else {
-   console.log("Nee");
+}
+ else {
+   console.log(false);
    alert("Jij mag stemmen met de verkiezingen")
    alert("Nee")
 }
